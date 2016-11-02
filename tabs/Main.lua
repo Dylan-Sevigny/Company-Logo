52a260c967487f01170566eddd6d9974626c9610
Dylan-Sevigny at codea-scm <Dylan-Sevigny@codea-scm.aws.mapote.com>
2016-11-02 15:03:39 +0000

    init

---
+++ tabs/Main.lua
@@ -0,0 +1,26 @@
+-- Company Logo
+
+-- Created by:Dylan Sevigny
+-- Created on:Oct-2016
+--Created for:ICS2O
+
+-- Use this function to perform your initial setup
+function setup()
+    supportedOrientations(LANDSCAPE_ANY)
+    displayMode(FULLSCREEN)
+    noFill()
+    noSmooth()
+    noStroke()
+    pushStyle()
+end
+
+-- This function gets called once every frame
+function draw()
+    -- This sets a dark background color 
+    background(40, 40, 50)
+
+    -- Do your drawing here
+    sprite("Project:CompanyLogoImage", WIDTH/2, HEIGHT/2, WIDTH, HEIGHT)
+    
+end
+
